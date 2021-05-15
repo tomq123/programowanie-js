fc6e077938ce746244a3caefd159ba09af0ca8db
Tomasz Kryń <tf.kryn@gmail.com>
2021-05-15 21:04:19 +0200

    Add simple js script

--- src/podstawy/script.js
+++ src/podstawy/script.js
@@ -1,1 +1,20 @@
-console.log('Hello World!');
+const element = document.getElementsByTagName('h1')[0]
+const pugins = navigator.plugins
+console.log('pugins', pugins)
+let playerName = ''
+const play = () => {
+    const isWin = Math.random() < 0.7
+    if(isWin) {
+        element.innerText = `${playerName}: Win!`
+    } else {
+        element.innerText = `${navigator.plugins}: Game over!`
+    }
+}
+const myInputElement = document.getElementById('myInput')
+myInputElement.addEventListener('input', (event) => {
+    playerName = event.target.value
+})
+const myButtonElement = document.getElementById('myButton')
+myButtonElement.addEventListener('click', (event) => {
+    play()
+})
